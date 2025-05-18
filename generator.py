f2c18687ca19804bb78dbba5a05a01583925db40
chc08rm <raminder.mulla@gmail.com>
2025-05-18 19:00:30 +0100

    basic implementation
    Have combined dataframing and reading segments of code.

--- generator.py
+++ generator.py
@@ -72,7 +72,7 @@ questionary.print("And finally for the collection, workup conditions and yield."
 aux_params=questionary.form(
     collection_into=questionary.text("What did you collect into? e.g. NH₄Cl, an inerted vial, etc."),
     collection_mode=questionary.select("What mode of collection did you use?", choices=["""Steady state — wait for at least 3   residence times of material to be passed through the reactor before collecting""", """Collecting all — collect all of the injectable quantity of limiting reagent after excess reagent lines have been primed""","Unknown"]),
-    product_1_smiles=questionary.text("Please enter the SMILES string for the major product obtained"),
+    product_1_smiles=questionary.text("Please enter the SMILES string for the major product obtained. If you don't know what it was (e.g. a complex mixture was formed), leave this blank."),
     product_1_yield=questionary.text("What was your yield?", validate=validate_float),
     product_1_yieldtype=questionary.select("How was this yield determined?", choices=["Weight","LCMS", "1H NMR", "Titration"])
 ).ask()
@@ -86,6 +86,37 @@ aux_params=pd.DataFrame([aux_params])
 reaction=pd.concat([pump_list,mixer_list,aux_params], axis=1)
 reaction.to_csv(f"{filename}.csv")
 #Here's the output
-
+print("In a flow reactor were combined",end=" ")
+print(f"""{reaction.iloc[0]["reagent_id"]} ({reaction.iloc[0]["reagent_eq"]} eq., {reaction.iloc[0]["concentration"]}M in {reaction.iloc[0]["solvent"]}) dosed in at a flow rate of {reaction.iloc[0]["flow_rate"]} mL min⁻¹ and {reaction.iloc[1]["reagent_id"]} ({reaction.iloc[1]["reagent_eq"]} eq., {reaction.iloc[1]["concentration"]}M in {reaction.iloc[1]["solvent"]}) dosed in at a flow rate of {reaction.iloc[1]["flow_rate"]} mL min⁻¹""", end=" ")
+#Mixer parameters for streams 1 and 2
+if reaction.iloc[1]['mixer_type']=="T-mixer":
+    print(f"""to a {reaction.iloc[1]['mixer_type']}(φ={reaction.iloc[1]['t_diam']} µm). The resulting mixture was held for a residence time of {reaction.iloc[1]['res_time']} s, prior to being""", end=" ")
+elif reaction.iloc[1]['mixer_type']=="CSTR":
+    print(f"""to a {reaction.iloc[1]['mixer_type']}. The resulting mixture was held for an MRT of {reaction.iloc[1]['res_time']} s, prior to being""", end=" ")
+else:
+    print(f"""to a {reaction.iloc[1]['mixer_type']}. The resulting mixture was held for a residence time of {reaction.iloc[1]['res_time']} s, prior to being""", end=" ")
+#now repeat for all other reagents and pumps in the flow. 
+for n in range(2, reaction.shape[0]):
+    print(f"""combined with {reaction.iloc[n]["reagent_id"]} ({reaction.iloc[n]["reagent_eq"]} eq.,{reaction.iloc[n]["concentration"]}M in {reaction.iloc[n]["solvent"]}) dosed in at a flow rate of {reaction.iloc[n]["flow_rate"]} mL min⁻¹""", end=" ")
+    if reaction.iloc[1]['mixer_type']=="T-mixer":
+        print(f"""to a {reaction.iloc[n]['mixer_type']}(φ={reaction.iloc[n]['t_diam']} µm). The resulting mixture was held for a residence time of {reaction.iloc[n]['res_time']} s, prior to being""", end=" ")
+    elif reaction.iloc[1]['mixer_type']=="CSTR":
+        print(f"""to a {reaction.iloc[n]['mixer_type']}. The resulting mixture was held for an MRT of {reaction.iloc[n]['res_time']} s, prior to being""", end=" ")
+    else:
+        print(f"""to a {reaction.iloc[n]['mixer_type']}. The resulting mixture was held for a residence time of {reaction.iloc[n]['res_time']} s, prior to being""", end=" ")
+    if n==reaction.index[-1]:
+        print(f"collected into {reaction.iloc[0]["collection_into"]}.")
+#And now for a statement of the collection behaviour.
+if reaction.iloc[0]["collection_mode"]=="STEADY_STATE":
+    print("Steady state collection was performed by infusing at least 3 residence times of all feed solutions through the reactor. Yields are reported on this basis.")
+elif reaction.iloc[0]["collection_mode"]=="COLLECT_ALL_PRIME":
+        print("All of the output following injection of the limiting reagent was collected", end=" ")
+        for n in range (0, reaction.shape[0]):
+            if reaction.iloc[n]['reagent_eq']>1:
+                print(f"The {reaction.iloc[n]['reagent_id']}, ", end=" ")
+        print("pump(s) were initiated and run for at least 20 s prior to initiation of the limiting reagent pump.")
+#yield readout
+if reaction.iloc[0]["product_1_yield"]>0:
+    print(f"{reaction.iloc[0]["product_1_smiles"]} was obtained in {reaction.iloc[0]["product_1_yield"]}% yield by {reaction.iloc[0]["product_1_yieldtype"]} measurement.")
 
         
